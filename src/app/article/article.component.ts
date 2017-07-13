343fc88979c5eb090dac48b4ecc9dd434aa374f3
SkyFather <cyh278889789@gmail.com>
2017-07-14 00:16:08 +0800

    update html

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,12 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    class: 'row'
+  }
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  votes: number;
+  title: string;
+  link: string;
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
 
+  }
+
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
   ngOnInit() {
   }
 
